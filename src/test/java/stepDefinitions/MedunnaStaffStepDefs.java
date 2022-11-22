82de09161d79d2e83c367e82769f6df24afe5d62
burak <karanfilkaranfil822@gmail.com>
2022-11-22 23:48:04 +0300

    23

--- src/test/java/stepDefinitions/MedunnaStaffStepDefs.java
+++ src/test/java/stepDefinitions/MedunnaStaffStepDefs.java
@@ -4,6 +4,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
@@ -41,8 +42,6 @@ public class MedunnaStaffStepDefs {
 
                 System.out.println(w.getText());
         }
-      //  actualData.forEach(t-> System.out.printf("%S, %S, %S, %S, %S, %S, %8S, %S, %S, %S, %S, %S, %S, %S, %S, %S, %S, %S",t.getText()));
-
         Assert.assertTrue(medunnaPage.hastaBilgileriTable.get(0).isDisplayed());
     }
 
@@ -59,30 +58,35 @@ public class MedunnaStaffStepDefs {
     @And("Id bilgisi duzenlenmedi")
     public void ıdBilgisiDuzenlenmedi() {
         ReusableMethods.waitForVisibility(medunnaPage.hastaBilgileriID,10);
-        actions.sendKeys(Keys.TAB).perform();
+        medunnaPage.hastaBilgileriID.sendKeys("2323");
+        String actualId=medunnaPage.hastaBilgileriID.getText();
+        assertFalse(actualId.contains("2323"));
 
     }
 
     @And("Hastanin diger bilgileri guncellendi")
     public void hastaninDigerBilgileriGuncellendi() throws InterruptedException {
-        actions.sendKeys(Keys.TAB).sendKeys("Berk").
-                sendKeys(Keys.TAB).sendKeys("Karanfil").
+        Driver.getDriver().navigate().refresh();
+        actions.sendKeys(medunnaPage.firstName).doubleClick().sendKeys("Adanali").
+                sendKeys(Keys.TAB).sendKeys("Turkiyeli").
                 sendKeys(Keys.TAB).sendKeys("02.02.1991").
                 sendKeys(Keys.RIGHT).sendKeys("23:23").
                sendKeys(Keys.TAB).sendKeys("bosversene@gmail.com").
                 sendKeys(Keys.TAB).sendKeys("5555555555").
                 sendKeys(Keys.TAB).sendKeys("Male").
                 sendKeys(Keys.TAB).sendKeys("A+").
-                sendKeys(Keys.TAB).sendKeys("sananeGardasYaw").
-                sendKeys(Keys.TAB).sendKeys("baksanaBana").
+                sendKeys(Keys.TAB).sendKeys("sananeGardasYaw").perform();
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        ReusableMethods.waitFor(2);
+        medunnaPage.description.clear();
+        ReusableMethods.waitFor(2);
+        actions.click(medunnaPage.description).sendKeys("banabuuggg").
                 sendKeys(Keys.TAB).sendKeys("hy").
-                sendKeys(Keys.TAB).sendKeys("Turkey ").
-                sendKeys(Keys.RIGHT).sendKeys(Keys.RIGHT).sendKeys(Keys.RIGHT).sendKeys(Keys.RIGHT).sendKeys(Keys.RIGHT).sendKeys(Keys.RIGHT).sendKeys(Keys.RIGHT).perform();
-
-        Thread.sleep(3000);
-        medunnaPage.state.sendKeys("Adana");
-        medunnaPage.saveButton.click();
-
+                sendKeys(Keys.TAB).sendKeys("Germany").perform();
+        ReusableMethods.waitFor(2);
+        medunnaPage.saveButton.submit();
+        ReusableMethods.waitForVisibility(medunnaPage.bilgilerGuncellendi,5);
+        assertTrue(medunnaPage.bilgilerGuncellendi.isDisplayed());
 
 
     }
@@ -99,11 +103,35 @@ public class MedunnaStaffStepDefs {
     @And("Tum bilgilerin dolduruldugu goruldu")
     public void tumBilgilerinDoldurulduguGoruldu() {
         assertFalse(medunnaPage.SSNBosmu.getText().isEmpty());
+        assertFalse(medunnaPage.firstNameBosmu.getText().isEmpty());
+        assertFalse(medunnaPage.lastNameBosmu.getText().isEmpty());
+        assertFalse(medunnaPage.birthDateBosmu.getText().isEmpty());
+        assertFalse(medunnaPage.emailBosmu.getText().isEmpty());
+        assertFalse(medunnaPage.genderBosmu.getText().isEmpty());
+        assertFalse(medunnaPage.bloodGroupBosmu.getText().isEmpty());
+        assertFalse(medunnaPage.addressBosmu.getText().isEmpty());
+        assertFalse(medunnaPage.descriptionBosmu.getText().isEmpty());
+        assertFalse(medunnaPage.createdDateBosmu.getText().isEmpty());
+        assertFalse(medunnaPage.userBosmu.getText().isEmpty());
+        assertFalse(medunnaPage.countryBosmu.getText().isEmpty());
+        assertTrue(medunnaPage.stateBosmu.getText().isEmpty());
     }
 
 
-
+    @And("Herhangi bir hasta bilgisi silindi")
+    public void herhangiBirHastaBilgisiSilindi() {
+        medunnaPage.description.clear();
+        ReusableMethods.waitFor(2);
+        medunnaPage.saveButton.submit();
+        ReusableMethods.waitForVisibility(medunnaPage.bilgilerGuncellendi,3);
+        assertTrue(medunnaPage.bilgilerGuncellendi.isDisplayed());
     }
+
+    @And("Herhangi bir hasta silinemedi")
+    public void herhangiBirHastaSilinemedi() {
+       assertFalse(medunnaPage.deleteYok.getText().contains("Delete"));
+    }
+}
 
 
 
--- src/test/resources/features/US09.feature
+++ src/test/resources/features/US09.feature
@@ -1,14 +1,13 @@
 Feature:US09 Staff (Personel),hasta bilgilerini görebilmeli, düzenleyebilmeli ama silememelidir.
 
   Scenario: TC001 "My Pages" sekmesinden hasta bilgilerini görebilmelidir.
-    Given logIn
+    Given logInStaf
     When My Pages sekmesine tiklar
     Then Search Patient secegine tiklar
     And Hasta bilgilerini gorur
 
   Scenario:TC002 Kullanıcı bütün hasta bilgilerini "id, firstname, lastname, birthdate, email, phone, gender, blood group,
   address, description, user, country and state/city" düzenleyebilmelidir."
-
 
     Then Hasta arama kutusuna hastanin "158-13-2323"'i girilir
     Then Edit butonuna tiklanir
@@ -18,13 +17,32 @@ Feature:US09 Staff (Personel),hasta bilgilerini görebilmeli, düzenleyebilmeli 
 
   Scenario: TC003 Kullanıcı, SSN'lerine göre yeni bir başvuru sahibi arayabilmeli ve tüm kayıt bilgilerinin doldurulduğunu görebilmelidir.
 
+   When My Pages sekmesine tiklar
+   Then Search Patient secegine tiklar
+   Then Hasta arama kutusuna hastanin "456-87-1234"'i girilir
+   And Wiev'e tiklanir
+   And Tum bilgilerin dolduruldugu goruldu
+
+  Scenario: TC004 Kullanıcı herhangi bir hasta bilgisini silebilir.
+
     When My Pages sekmesine tiklar
     Then Search Patient secegine tiklar
-    Then Hasta arama kutusuna hastanin "158-13-2323"'i girilir
-    And Wiev'e tiklanir
-    And Tum bilgilerin dolduruldugu goruldu
+    And Hasta arama kutusuna hastanin "456-87-1234"'i girilir
+    And Edit butonuna tiklanir
+    And Herhangi bir hasta bilgisi silindi
 
+  Scenario: TC005  Staff hastaları silememeli.
 
+    When My Pages sekmesine tiklar
+    Then Search Patient secegine tiklar
+    And Hasta arama kutusuna hastanin "456-87-1234"'i girilir
+    And Herhangi bir hasta silinemedi
+
+  Scenario: TC006  Admin, hastaları SSN kimliklerine göre arayamadi.
+
+    Given logInAdmin
+    Then Search Patient secegine tiklar
+     And Admin, hastaları SSN kimliklerine göre arayamadi.
 
 
 
